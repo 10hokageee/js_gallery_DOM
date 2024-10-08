9ed7659cd17a47c2b55b4700c91b0bd4efb2989c
Nikita Serdyukov <nikita.serdyukov.20033002@gmail.com>
2024-10-08 16:27:04 +0300

    make build

---
+++ index.184a957f.js
@@ -0,0 +1,2 @@
+var e=document.querySelectorAll(".gallery__thumb"),t=document.getElementById("largeImg");e.forEach(function(e){e.addEventListener("click",function(e){e.preventDefault();var l=e.target.closest(".list-item__link");l&&(t.src=l.href)})});
+//# sourceMappingURL=index.184a957f.js.map
